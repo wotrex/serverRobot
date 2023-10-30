64f1180dd64906f83cbc099083af76512331e431
wotrex <47450238+wotrex@users.noreply.github.com>
2023-10-30 13:49:47 +0200

    camera_back

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from flask import Flask, request, render_template, Response, jsonify
 
 
 frame = None
-close = False
+frame2 = None
 
 ##############################################Flask########################
 app = Flask(__name__)
@@ -17,6 +17,16 @@ def main():
     while True:
         if frame is not None:
             ret, buffer = cv2.imencode('.jpg', frame)
+            image = buffer.tobytes()
+            yield (b'--frame\r\n'
+            
+                            b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
+            
+def main2():
+    global frame2
+    while True:
+        if frame2 is not None:
+            ret, buffer = cv2.imencode('.jpg', frame2)
             image = buffer.tobytes()
             yield (b'--frame\r\n'
             
@@ -34,6 +44,12 @@ def index():
 def video():
 
     return Response(main(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video2')
+
+def video2():
+
+    return Response(main2(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/gamepad_data', methods=['POST'])
 def gamepad_data():
@@ -57,17 +73,13 @@ def generate_unique_client_id():
 
 async def send_data_via_ws(client_id, data):
     ws = websocket_connections.get(client_id)
-    
     await ws.send_json(data)
 
-async def handle(request):
+async def handleVideo(request):
     global frame
     ws = WebSocketResponse()
     await ws.prepare(request)
-    client_id = generate_unique_client_id()  # Генерувати унікальний ідентифікатор для кожного клієнта
-    websocket_connections[client_id] = ws  # Зберігати посилання на WebSocket з'єднання
-    print('Client connected: ' + client_id)
-
+    print('Client connected: ' + request.remote)
 
     while True:
         msg = await ws.receive()
@@ -78,16 +90,50 @@ async def handle(request):
 ##            cv2.imshow('Server Video', frame)
 ##            cv2.waitKey(1)
         elif msg.type in (WSMsgType.CLOSE, WSMsgType.CLOSED, WSMsgType.CLOSING):
-            del websocket_connections[client_id]  # При завершенні з'єднання видаляємо його зі словника
-            print('Client disconnected: ' + client_id)
+            print('Client disconnected: ' + request.remote)
             break
 
     return ws
 
+async def handleVideo2(request):
+    global frame2
+    ws = WebSocketResponse()
+    await ws.prepare(request)
+    print('Client connected: ' + request.remote)
+
+    while True:
+        msg = await ws.receive()
+        if msg.type == WSMsgType.binary:
+            frame_data = msg.data
+            frame2 = cv2.imdecode(np.frombuffer(frame_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+            # Отримані кадри можна відображати, наприклад, за допомогою OpenCV
+##            cv2.imshow('Server Video', frame)
+##            cv2.waitKey(1)
+        elif msg.type in (WSMsgType.CLOSE, WSMsgType.CLOSED, WSMsgType.CLOSING):
+            print('Client disconnected: ' + request.remote)
+            break
+
+    return ws
+
+async def send_handler(request):
+    ws = WebSocketResponse()
+    await ws.prepare(request)
+    client_id = generate_unique_client_id()  # Генерувати унікальний ідентифікатор для кожного клієнта
+    websocket_connections[client_id] = ws  # Зберігати посилання на WebSocket з'єднання
+    print('Client connected: ' + request.remote)
+    while True:
+        msg = await ws.receive()
+        if msg.type in (WSMsgType.CLOSE, WSMsgType.CLOSED, WSMsgType.CLOSING):
+            del websocket_connections[client_id]
+            print('Client disconnected: ' + request.remote)
+            break
+    return ws
 
 if __name__ == '__main__':
     t1 = Thread(target = start_flask)
     t1.start()
     WebApp = web.Application()
-    WebApp.router.add_get('/ws', handle)
+    WebApp.add_routes([web.get('/ws', handleVideo),
+                web.get('/send', send_handler),
+                web.get('/ws2', handleVideo2)])
     web.run_app(WebApp)
